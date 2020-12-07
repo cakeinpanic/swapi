43980815e1c3e5dba11abc4ce7f4405abffdadba
cakeinpanic <cakeinpanic@gmail.com>
2020-12-07 21:21:51 +0200

    add animation

--- src/app/planet-chart/chart.service.ts
+++ src/app/planet-chart/chart.service.ts
@@ -1,12 +1,37 @@
 import { Injectable } from '@angular/core';
 import { forkJoin, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { ApiService, IPlanet } from '../api.service';
+
+export interface IChartData {
+  value: number;
+  label: string;
+  name: string;
+}
+
+const MIN_BAR_HEIGHT = 10;
 
 @Injectable({ providedIn: 'root' })
 export class ChartService {
   constructor(private apiService: ApiService) {}
 
-  getPlanetsInfo(planetUrls: string[]): Observable<IPlanet[]> {
+  private getPlanetsChartInfo(planetUrls: string[]): Observable<IPlanet[]> {
     return forkJoin(...planetUrls.map(url => this.apiService.getPlanet(url)));
+  }
+
+  getChartsData(planetUrls: string[]): Observable<IChartData[]> {
+    return this.getPlanetsChartInfo(planetUrls).pipe(
+      map(data => {
+        const max = Math.max(...data.map(({ population }) => +population));
+
+        return data.map(({ population, name }) => {
+          let value = (+population / max) * 100;
+          if (value <= MIN_BAR_HEIGHT) {
+            value += MIN_BAR_HEIGHT;
+          }
+          return { name, value, label: population };
+        });
+      })
+    );
   }
 }
--- src/app/planet-chart/planet-chart.component.ts
+++ src/app/planet-chart/planet-chart.component.ts
@@ -1,41 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { ChartService } from './chart.service';
-
-interface IChartData {
-  value: number;
-  label: string;
-  name: string;
-}
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { mapTo, startWith } from 'rxjs/operators';
+import { ChartService, IChartData } from './chart.service';
 
 @Component({
   selector: 'app-planet-chart',
   templateUrl: './planet-chart.component.html',
   styleUrls: ['./planet-chart.component.scss'],
 })
-export class PlanetChartComponent implements OnInit {
-  planetData: IChartData[] = [];
-  isLoaded = false;
+export class PlanetChartComponent {
+  planetData$: Observable<IChartData[]> = this.chartService.getChartsData([
+    'http://swapi.dev/api/planets/1/',
+    'http://swapi.dev/api/planets/2/',
+    'http://swapi.dev/api/planets/6/',
+    'http://swapi.dev/api/planets/7/',
+    'http://swapi.dev/api/planets/8/',
+  ]);
+
+  isLoaded$: Observable<boolean> = this.planetData$.pipe(mapTo(true), startWith(false));
 
   constructor(private chartService: ChartService) {}
-
-  async ngOnInit() {
-    this.isLoaded = false;
-    this.chartService
-      .getPlanetsInfo([
-        'http://swapi.dev/api/planets/1/',
-        'http://swapi.dev/api/planets/2/',
-        'http://swapi.dev/api/planets/6/',
-        'http://swapi.dev/api/planets/7/',
-        'http://swapi.dev/api/planets/8/',
-      ])
-      .subscribe(data => {
-        const max = Math.max(...data.map(({ population }) => +population));
-        this.planetData = data.map(({ population, name }) => {
-          const value = +population;
-          return { name, value: (value / max) * 100, label: population };
-        });
-
-        this.isLoaded = true;
-      });
-  }
 }
