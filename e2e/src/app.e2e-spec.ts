d75a3afc102ae9176aaa332bf7f21768f0add8dc
cakeinpanic <cakeinpanic@gmail.com>
2020-12-07 21:43:21 +0200

    add gh-pages deployment

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('tikal app is running!');
+    expect(page.getTitleText()).toEqual('swapi app is running!');
   });
 
   afterEach(async () => {
